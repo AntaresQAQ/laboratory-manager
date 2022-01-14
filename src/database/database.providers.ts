a58e4d95dd41a2060144d1c64394f992ac15c8ef
Antares <antares.oier@gmail.com>
2022-01-14 08:34:17 +0800

    1

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -3,14 +3,8 @@ import { ConfigService } from '@/config/config.service';
 export const databaseProviders = [
   TypeOrmModule.forRootAsync({
     useFactory: (configService: ConfigService) => ({
-      type: configService.config.database.type,
-      host: configService.config.database.host,
-      port: configService.config.database.port,
-      username: configService.config.database.username,
-      password: configService.config.database.password,
-      database: configService.config.database.database,
+      ...configService.config.database,
       entities: [`${__dirname}/../**/*.entity{.ts,.js}`],
-      logging: configService.config.database.logging,
       synchronize: true,
       charset: 'utf8mb4',
     }),
